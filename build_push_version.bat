db48f9305e580709712fc1fcc0c5f565e27d1bb7
nimigeanconstantinion <constantin.nimigean@gmail.com>
2025-03-06 11:59:58 +0200

    commit 06032025 11.59

--- build_push_version.bat
+++ build_push_version.bat
@@ -11,7 +11,7 @@ echo "Creating and using Docker Buildx builder..."
 docker buildx create --use || docker buildx use default
 
 echo "Building and pushing multi-platform Docker image..."
-docker buildx build --platform linux/amd64,linux/arm64 -f Dockerfile -t ion21/kube-ms1:%SERVICE_VERSION% --push .
+docker buildx build --platform linux/amd64,linux/arm64 -f Dockerfile -t mycodeschool/kube-ms1:%SERVICE_VERSION% --push .
 @REM docker buildx build --platform linux/amd64 -f Dockerfile -t ion21/kube-ms1:%SERVICE_VERSION% --push .
 
 echo "Script completed."
